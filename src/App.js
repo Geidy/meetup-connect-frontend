7fe48fb8a5d2c47de2e2dd52d1cdd75ce86e3a98
Geidy <geidydm@gmail.com>
2019-08-04 21:08:53 -0400

    imported Navbar and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
+import Navbar from './components/Navbar';
 
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Header />
         <Main/>
       </div>
