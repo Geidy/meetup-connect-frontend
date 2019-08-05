3b9e6afb80479d2e145dd2efba821ca9e49376ad
Geidy <geidydm@gmail.com>
2019-08-04 21:05:43 -0400

    Created a Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../css/Navbar.css";
+
+function Navbar() {
+    return(
+        <div className="Navbar">
+            <a class="navbar-brand" href="#">MeetUp Connect</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  
+        </div>
+    )
+
+}
+
+export default Navbar;
